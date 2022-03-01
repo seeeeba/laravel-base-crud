b0c0313d627b8d6b9ab864b867528301bf45447f
seeeeba <lillo.sebastian1@gmail.com>
2022-03-01 18:34:09 +0100

    layout + bootstrap

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Lista dei fumetti</h1>
+@extends('layouts.app')
+
+@section('main_content')
+<h1>Lista dei fumetti</h1>
 
     @foreach ($comics as $comic)
         <div>
@@ -16,6 +9,4 @@
             <div>{{$comic->price}}</div>
         </div>
     @endforeach
-
-</body>
-</html>
+@endsection
